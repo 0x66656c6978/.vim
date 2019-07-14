9c9faa977271ff2721112cde4ddb8664f3de4e48
0x66656c6978 <0x66656c6978@gmail.com>
2019-07-14 03:10:54 +0200

    add snipmate, delimitMate, fugitive and fzf / fzf.vim plugins

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 P_=`pwd`
 
+if [ ! -d $HOME/.fzf ] ; then
+    echo Installing fzf
+    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+    ~/.fzf/install
+fi
+
 echo Installing pathogen
 mkdir -p $HOME/.vim/autoload $HOME/.vim/bundle 
 curl -LSso $HOME/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
@@ -14,9 +20,8 @@ if [ -f $HOME/.vimrc ] ; then
     echo Creating backup of $HOME/.vimrc in $HOME/.vimrc~
 fi
 
+
 echo Creating / Overwriting $HOME/.vimrc
 cd "$P_"
 cp .vimrc $HOME/.vimrc
-
-
 echo 'source "$HOME/.vim/bundle/gruvbox/gruvbox_256palette.sh"' >> ~/.bashrc
