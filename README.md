631415c6080d94f3959d8c9f9b21fbbecdb2e4ee
0x66656c6978 <0x66656c6978@gmail.com>
2019-07-18 20:02:31 +0200

    improve setup

--- README.md
+++ README.md
@@ -4,8 +4,5 @@ my personal .vim folder
 ## setup
 
 ```
-cd $HOME
-git clone https://github.com/0x66656c6978/.vim.git
-cd .vim
-./setup.sh
+git clone https://github.com/0x66656c6978/.vim.git "$HOME/.vim" && "$HOME/.vim/setup.sh"
 ```
--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-P_=`pwd`
+
+BASEDIR=$(dirname "$0")
 
 if [ ! -d $HOME/.fzf ] ; then
     echo Installing fzf
@@ -8,23 +9,19 @@ if [ ! -d $HOME/.fzf ] ; then
 fi
 
 echo Installing pathogen
-if [ ! -d $HOME/.vim/autoload ] || [ ! -d $HOME/.vim/bundle ]; then
-    mkdir -p $HOME/.vim/autoload $HOME/.vim/bundle 
-    curl -LSso $HOME/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+if [ ! -d "$HOME/.vim/autoload" ] || [ ! -d "$HOME/.vim/bundle" ]; then
+    mkdir -p "$HOME/.vim/autoload" "$HOME/.vim/bundle"
+    curl -LSso "$HOME/.vim/autoload/pathogen.vim" "https://tpo.pe/pathogen.vim"
 fi
 
 echo Installing pathogen bundles from git repositories to $HOME/.vim/bundle
-cd $HOME/.vim/bundle
-cat "$P_"/.bundle-list | while read -r URL; do git clone $URL; done
+cd "$HOME/.vim/bundle"
+cat "$BASEDIR/.bundle-list" | while read -r URL; do git clone $URL; done
 
 if [ -f $HOME/.vimrc ] ; then
-    mv $HOME/.vimrc $HOME/.vimrc~
+    mv "$HOME/.vimrc" "$HOME/.vimrc~"
     echo Creating backup of $HOME/.vimrc in $HOME/.vimrc~
 fi
 
 echo Creating / Overwriting $HOME/.vimrc
-cd "$P_"
-cp .vimrc $HOME/.vimrc
-
-
-echo 'source "$HOME/.vim/bundle/gruvbox/gruvbox_256palette.sh"' >> $HOME/.bashrc
+cp "$BASEDIR/.vimrc" "$HOME/.vimrc"
